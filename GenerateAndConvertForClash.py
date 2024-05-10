bd6aa41156138476d3e338b3ad0a9451b1ac5f34
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2024-05-10 14:53:49 +0800

    Update GenerateAndConvertForClash.py

--- GenerateAndConvertForClash.py
+++ GenerateAndConvertForClash.py
@@ -29,7 +29,7 @@ def main(source="./data", output="./clash_Rule_Set"):
             exportList = exportList + sourceDir[i]
         else:
             exportList = exportList + sourceDir[i] + ","
-    os.system("go run ./ --outputdir=" + output + " --exportlists=" + exportList)
+    os.system("go run ./ --datapath=" + source + " --outputdir=" + output + " --exportlists=" + exportList)
     print("\n\n")
     for i in sourceDir:
         i = i.lower()
